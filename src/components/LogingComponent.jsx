435f6666e8bb40fc2deb3dbee0c96c8199345e9b
DaniCode <82921747+DanielaCode@users.noreply.github.com>
2023-08-07 17:38:59 +0000

    Initial structure!

---
+++ src/components/LogingComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LogingComponent() {
+  return (
+    <div>Loging Component</div>
+  )
+}
+
+export default LogingComponent
---
+++ src/components/TodoComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import LogingComponent from './LogingComponent'
+import WelcomeComponent from './WelcomeComponent'
+
+function TodoComponent() {
+  return (
+    <div>
+        TodoComponent
+        <LogingComponent/>
+        <WelcomeComponent/>
+    </div>
+  )
+}
+
+export default TodoComponent
