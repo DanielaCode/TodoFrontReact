80098c9f2a52a5092fff931ed7e249e2cc00edb1
DaniCode <82921747+DanielaCode@users.noreply.github.com>
2023-08-08 21:49:02 +0000

    Todo list dummy info done

--- src/components/TodosListComponent.jsx
+++ src/components/TodosListComponent.jsx
@@ -1,12 +1,55 @@
-import React from 'react'
+import React from "react";
 
 function TodosListComponent() {
+  const todos = [
+    {
+      id:1,
+      des:"study microservices",
+      sts:"done",
+      date: new Date(2023,1,6)
+    },
+    {
+      id:2,
+      des:"study microservices",
+      sts:"done",
+      date: new Date(2023,1,6)
+    },
+    {
+      id:3,
+      des:"study microservices",
+      sts:"done",
+      date: new Date(2023,1,6)
+    }
+  ]
+
+  const items= todos.map((todo)=>(
+    <tr key={todo.id}>
+      <td>{todo.id}</td>
+      <td>{todo.des}</td>
+      <td>{todo.sts}</td>
+      <td>{todo.date.toDateString()}</td>
+
+    </tr>
+ ))  
   return (
-    <div className='TodosListComponent'>
-        <h1>Todos List</h1>
-        TodosListComponent
+    <div className="TodosListComponent">
+      <h1>Todos List</h1>
+      TodosListComponent
+      <table>
+        <thead>
+          <tr>
+            <td>Id</td>
+            <td>Description</td>
+            <td>Status</td>
+            <td>Date</td>
+          </tr>
+        </thead>
+        <tbody>
+          {items}
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
 
-export default TodosListComponent
+export default TodosListComponent;
