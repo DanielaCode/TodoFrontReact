c639cf6bc88f4ca0e5d87582818863366b390177
Daniela <danielagp96@gmail.com>
2023-09-08 08:25:00 -0600

    Adding Yup to validate login form and changing some styles

--- src/components/LogingComponent.jsx
+++ src/components/LogingComponent.jsx
@@ -1,43 +1,48 @@
-import { useState } from 'react'
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../providers/AuthContext";
 import {Formik,Form,Field,ErrorMessage} from 'formik';
+import * as Yup from 'yup';
+
+const SingupSchema= Yup.object().shape({
+  username: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  password: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+});
+
 
 function LogingComponent() {
   const navigate = useNavigate();
   const auth = useAuth();
-  const [username,setUsername] = useState("");
-  const [password,setPassword] = useState("");
   async function handleSubmit(event) {
     console.log(event)
     if (await auth.login(event.username,event.password)) {
       navigate(`/welcome/${event.username}`)
     }
   }
-  function validate(values){
-    const errors={}
-    if(values.username.length<5){
-      errors.username="at least 5 characters"
-    }
-    return errors;
-  }
+  
   return (
     <div className="LogingComponent">
       <Formik  
-        initialValues={{username,password}} 
+        initialValues={{username:"",password:""}} 
         enableReinitialize={true} 
         onSubmit={handleSubmit}
-        validate={validate}>
-        <Form className='m-5'>
-          <h1>Login</h1>
+        validationSchema={SingupSchema}>
+        <Form className='container w-25'>
+          <h1 className="m-5">Login</h1>
           {/* {!auth.isAuth && <div className='alert alert-warning'>Username or password incorrect</div>} */}
-          <ErrorMessage name='username' component='div' className='alert alert-warning'/>
-          <fieldset className='form-group'>
-            <label htmlFor="username">username:</label>
+          <ErrorMessage name='username' component='div' className='alert alert-warning'/> 
+          <ErrorMessage name='password' component='div' className='alert alert-warning'/>
+          <fieldset className='form-group mb-3'>
+            <label className="p-0 m-0 w-100" htmlFor="username">Username:</label>
             <Field type="text" name="username" className="form-control"></Field>
           </fieldset>
           <fieldset className='form-group'>
-            <label htmlFor="password">password:</label>
+            <label className="p-0 m-0 w-100" htmlFor="password">Password:</label>
             <Field type="password" name="password" className="form-control"></Field>
           </fieldset>
           <div>
